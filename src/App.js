0f5fb6f374db32264ab97ed425adf07d9652a172
Prathamesh Kulkarni <p.kulkarni@axelor.com>
2024-05-05 19:01:58 +0530

    File structure created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TopBar from "./component/TopBar";
+import Filters from "./component/Filters";
+import JobsContainer from "./component/JobsContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopBar />
+      <Filters />
+      <JobsContainer />
     </div>
   );
 }
---
+++ src/component/JobsContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import JobCard from "./JobCard";
+
+const JobsContainer = () => {
+  return (
+    <div>
+      <JobCard />
+    </div>
+  );
+};
+
+export default JobsContainer;
